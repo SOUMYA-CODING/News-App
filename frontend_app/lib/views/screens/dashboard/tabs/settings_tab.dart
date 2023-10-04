465857d88a5e02d5d7bb3dc6bf6f22bf40422d11
Soumya Prakash Sahu <52891015+SOUMYA-CODING@users.noreply.github.com>
2023-10-04 13:57:12 +0530

    added admin site

--- frontend_app/lib/views/screens/dashboard/tabs/settings_tab.dart
+++ frontend_app/lib/views/screens/dashboard/tabs/settings_tab.dart
@@ -5,7 +5,7 @@ class SettingsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return const Column(
       children: [
         Text("Hello Settings")
       ],
