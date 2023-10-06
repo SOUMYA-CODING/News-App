757658b6db3ed9115936aab146996440fd56ca07
Soumya Prakash Sahu <52891015+SOUMYA-CODING@users.noreply.github.com>
2023-10-06 20:48:18 +0530

    change in url

--- backend_api/backend_api/urls.py
+++ backend_api/backend_api/urls.py
@@ -5,7 +5,6 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -23,5 +22,3 @@ urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += staticfiles_urlpatterns()
